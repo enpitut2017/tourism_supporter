0776fe90e2e02bc8dfeef19373b5cfc7c8b21a9c
村本尚生 <naoki@muramotohisaonoMacBook-Pro.local>
2017-08-02 14:03:42 +0900

    一意性，存在性の検証

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -1,7 +1,32 @@
 require 'test_helper'
 
 class SpotTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @spot = Spot.new(placeName: "Example Spot", lat: 0, lng: 0)
+  end
+
+  test "should be valid" do
+    assert @spot.valid?
+  end
+
+  test "placeName should be present" do
+    @spot.placeName = ""
+    assert_not @spot.valid?
+  end
+
+  test "lat should be present" do
+    @spot.lat = nil
+    assert_not @spot.valid?
+  end
+
+  test "lng should be present" do
+    @spot.lng = ""
+    assert_not @spot.valid?
+  end
+
+  test "placeName should be unique" do
+    duplicate_user = @spot.dup
+    @spot.save
+    assert_not duplicate_user.valid?
+  end
 end
