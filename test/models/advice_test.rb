81ca8433fc5b12aa1e2bea5be474db1f2308a73f
fujita takato <takato@enpit.local>
2017-08-02 16:54:15 +0900

    ci用の修正7

--- test/models/advice_test.rb
+++ test/models/advice_test.rb
@@ -3,8 +3,8 @@ require 'test_helper'
 class AdviceTest < ActiveSupport::TestCase
   def setup
     @spot = spots(:one)
-    @advice = @spot.advices.build(pictureURL: "a", comment: "aa")
-    #@advice = Advice.new(spot_id:@spot.id, pictureURL: "a", comment: "aa")
+   # @advice = @spot.advices.build(pictureURL: "a", comment: "aa")
+    @advice = Advice.new(spot_id:@spot.id, pictureURL: "a", comment: "aa")
   end
 
   test "should be valid" do
