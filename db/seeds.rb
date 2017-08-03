622a52669049cfd4a048e5468817081635db62ba
Ryota Shimomura <s1713111@s.tsukuba.ac.jp>
2017-08-03 16:04:00 +0900

    spotのtableにpictureからむを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,14 @@
 Spot.create!(placeName:  "Funsui",
              lat: 38,
-             lng: 140)
+             lng: 140,
+             picture: 'http://kobe.travel.coocan.jp/photo/kobe/sumarikyuu/funsui/funsui_003.jpg')
 
 Spot.create!(placeName:  "Mt.tsukuba",
              lat: 39.1,
-             lng: 140.1)
+             lng: 140.1,
+             picture: 'http://www.ibarakiguide.jp/wp-content/uploads/2013/02/tsukuba3.jpg')
 
 Spot.create!(placeName:  "Ushikudaibutsu",
              lat: 37.9,
-             lng: 139.8)
+             lng: 139.8,
+             picture: 'http://blog-imgs-92.fc2.com/m/i/r/miraimiku1202/0516-0.jpg')
