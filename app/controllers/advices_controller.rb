1bced7b92cf1597165bf7ef48f47702c6c4e630b
Shizuma <s1511536@u.tsukuba.ac.jp>
2017-08-03 14:41:29 +0900

    ワンポイントアドバイス送信フォームの作成

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,5 +1,15 @@
 class AdvicesController < ApplicationController
   def new
-    @advice = 
+    @advice = Advice.new
+  end
+
+  def create
+    @advice = Advice.new(advice_params)
+    @advice.save
+    redirect_to advices_new_path
+  end
+
+  def advice_params
+    params.require(:advice).permit(:spot_id, :pictureURL, :comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  get 'advices/new'
+  root 'spots#show'
 
-  get 'spots/show'
+  get 'advices/new', to: 'advices#new'
+
+  get 'spots/show', to: 'spots#show'
+
+  resources :advices
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
