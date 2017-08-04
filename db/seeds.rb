0d0354b14d602eeaef5b62476fa3cc866281d135
Muramoto Naoki <naoki.muramoto@gmail.com>
2017-08-04 12:43:29 +0900

    sampleデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,26 @@
-Spot.create!(placeName:  "Funsui",
-             lat: 38,
-             lng: 140,
-             picture: 'http://kobe.travel.coocan.jp/photo/kobe/sumarikyuu/funsui/funsui_003.jpg')
+Spot.create!(placeName:  "筑波大学嘉納治五郎像",
+             lat: 36.105469,
+             lng: 140.102282,
+             picture: '/sample1.jpg')
 
-Spot.create!(placeName:  "Mt.tsukuba",
-             lat: 39.1,
-             lng: 140.1,
-             picture: 'http://www.ibarakiguide.jp/wp-content/uploads/2013/02/tsukuba3.jpg')
+Spot.create!(placeName:  "附属病院けやき棟から",
+             lat: 36.092247,
+             lng: 140.102209,
+             picture: '/sampletsukuba.jpg')
 
-Spot.create!(placeName:  "Ushikudaibutsu",
-             lat: 37.9,
-             lng: 139.8,
-             picture: 'http://blog-imgs-92.fc2.com/m/i/r/miraimiku1202/0516-0.jpg')
+Spot.create!(placeName:  "一の矢エリアの並木道",
+             lat: 36.119330,
+             lng: 140.099204,
+             picture: '/sampletsukuba2.jpg')
+
+Advice.create!(spot_id: 1,
+               pictureURL: '/sample1.jpg',
+               comment: "斜め下から撮りましょう")
+
+Advice.create!(spot_id: 2,
+               pictureURL: '/sampletsukuba.jpg',
+               comment: "黄昏時の大学と筑波山。夕暮れ時がおすすめです。")
+
+Advice.create!(spot_id: 3,
+               pictureURL: '/sampletsukuba2.jpg',
+               comment: "夏、よく晴れた日に撮りましょう！")
