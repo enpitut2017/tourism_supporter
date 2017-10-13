db9ce8102c39657824970577deba6299a623ff88
Shizuma <kirua515@gmail.com>
2017-10-13 19:48:46 +0900

    routes.rbにloginページのルートを追加、new.html.erbの編集、User_login_testの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-
-  get 'sessions/new'
-
   root 'spots#index'
 
   resources :spots do
     resources :advices, shallow: true
   end
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
 end
