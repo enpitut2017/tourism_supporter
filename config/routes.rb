7ea6531b6d73b212caa9de52a565eacb44cf0fab
tomohiro-matsushita <tm.5040.tk@gmail.com>
2017-10-13 17:46:17 +0900

    新規ユーザ作成ページへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'spots#index'
 
   resources :spots do
     resources :advices, shallow: true
   end
+
+  get  '/signup',  to: 'users#new'
 
 end
