767753116c3091f3ecc1764c23007727e263ab3d
Shizuma <kirua515@gmail.com>
2017-10-18 14:55:56 +0900

    users_login_testでredirect先のパスが間違っていたのを修正

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -11,9 +11,9 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     post login_path, params: { session: { email:    @user.email,
                                           password: 'password' } }
-    assert_redirected_to @user
+    assert_redirected_to root_path
     follow_redirect!
-    assert_template 'users/show'
+    assert_template '/'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
