39ef1fd50afbe2db80b5c3005cf6f830c582cdaa
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-20 17:08:13 +0900

    リファクタリング

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,12 @@
 class LikesController < ApplicationController
   def create
-    @like = Like.create(user_id: current_user.id, advice_id: params[:advice_id])
-    @likes = Like.where(advice_id: params[:advice_id])
-    @advices = Advice.all
+    Like.create(user_id: current_user.id, advice_id: params[:advice_id])
     @advice = Advice.find(params[:advice_id])
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id, advice_id: params[:advice_id])
+    like = Like.find(params[:id])
+    @advice = like.advice
     like.destroy
-    @likes = Like.where(advice_id: params[:advice_id])
-    @advices = Advice.all
-    @advice = Advice.find(params[:advice_id])    
   end
 end
