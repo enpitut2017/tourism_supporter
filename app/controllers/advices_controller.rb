e843f0f39492f50fb48823de5361bef23c6b32bc
fujita takato <takato@13351126194.ap.cc.tsukuba.ac.jp>
2017-10-20 16:05:59 +0900

    アドバイスの削除を実装

--- app/controllers/advices_controller.rb
+++ app/controllers/advices_controller.rb
@@ -1,6 +1,7 @@
 class AdvicesController < ApplicationController
   include AdvicesHelper
   before_action :logged_in_user, only: [:new, :create]
+  before_action :correct_user,   only: :destroy
 
   def new
     @spot = Spot.find(params[:spot_id])
@@ -23,7 +24,18 @@ class AdvicesController < ApplicationController
     @advice = Advice.find(params[:id])
   end
 
+  def destroy
+    @advice.destroy
+    flash[:success] = "advide deleted"
+    redirect_to root_url
+  end
+
   def advice_params
     params.require(:advice).permit(:spot_id, :picture, :comment)
   end
+
+  def correct_user
+      @advice = current_user.advices.find_by(id: params[:id])
+      redirect_to root_url if @advice.nil?
+    end
 end
