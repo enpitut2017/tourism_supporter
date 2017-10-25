4018266fc665b883c23557408eb21534a7055df1
fujita takato <takato@13351122061.ap.cc.tsukuba.ac.jp>
2017-10-25 13:58:55 +0900

    テストの追加・修正

--- test/controllers/advices_controller_test.rb
+++ test/controllers/advices_controller_test.rb
@@ -25,4 +25,65 @@ class AdvicesControllerTest < ActionDispatch::IntegrationTest
     end
     assert_redirected_to root_url
   end
+
+  test "should redirect destroy for correct advice" do
+    log_in_as(@user)
+    advice = advices(:two)
+    assert_difference 'Advice.count', -1 do
+      delete advice_path(advice)
+    end
+    assert_redirected_to controller: :spots, action: :show
+  end
+
+  test "should not destroy without login" do
+    advice = advices(:one)
+    assert_no_difference 'Advice.count' do
+      delete advice_path(advice)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect edit for wrong advice" do
+    log_in_as(users(:ants))
+    advice = advices(:one)
+    spot  = spots(:one)
+    comment =  "test"
+    user = @user
+    assert_no_difference 'Advice.count' do
+      patch advice_path(advice), params: { one: { spot: spot,
+                                                  comment: comment,
+                                                  user: user } }
+    end
+    assert_redirected_to root_url
+  end
+
+  test "should redirect edit for correct advice" do
+    log_in_as(@user)
+    advice = advices(:one)
+
+    spot  = spots(:one)
+    comment =  "test"
+    user = @user
+    patch advice_path(advice), params: { advice: { spot: spot,
+                                                comment: comment,
+                                                user: user } }
+    assert_redirected_to controller: :advices, action: :show
+    advice.reload
+    assert_equal spot,  advice.spot
+    assert_equal comment,  advice.comment
+    assert_equal user,  advice.user
+  end
+
+  test "should not edit without login" do
+    advice = advices(:one)
+    spot  = spots(:one)
+    comment =  "test"
+    user = @user
+    assert_no_difference 'Advice.count' do
+      patch advice_path(advice), params: { one: { spot: spot,
+                                                  comment: comment,
+                                                  user: user } }
+    end
+    assert_redirected_to login_url
+  end
 end
