2de9b14d5b1e2673e21b74ec8569eade660e754c
Shizuma <kirua515@gmail.com>
2017-10-27 17:09:43 +0900

    スポットの表示のパーシャル作成 リファクタリング

--- app/controllers/mylists_controller.rb
+++ app/controllers/mylists_controller.rb
@@ -5,7 +5,6 @@ class MylistsController < ApplicationController
     if logged_in?
       @user = current_user
       @mylist = @user.mylists
-      @spots = Spot.all
     else
       redirect_to login_path
     end
