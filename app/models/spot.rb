f592836588dc1aab28de4645f81e3a7770564a15
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-10-27 17:52:52 +0900

    いいねが一番多い画像を選択

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -6,11 +6,25 @@ class Spot < ApplicationRecord
             uniqueness: true
   validates :lat, presence: true
   validates :lng, presence: true
+
   def self.search(search) #ここでのself.はUser.を意味する
     if search
       where(['place_name LIKE(?)', "%#{search}%"]) #検索とnameの部分一致を表示。User.は省略
     else
       all #全て表示。User.は省略
+    end
+  end
+
+  def self.default_picture
+    "no_image.png"
+  end
+
+  def picture
+    advice = advices.where.not(picture: nil).order(likes_count: :desc).first
+    if advice.present?
+      advice.picture
+    else
+      Spot.default_picture
     end
   end
 end
