24ed546a889437139a83b6df82b1c2714e434c5b
Shizuma <kirua515@gmail.com>
2017-12-06 13:46:39 +0900

    ログインしているユーザ以外でコメントを消せなくなった テスト追加

--- test/integration/comment_create_test.rb
+++ test/integration/comment_create_test.rb
@@ -7,6 +7,7 @@ class CommentCreateTest < ActionDispatch::IntegrationTest
     @comment = @advice.comments.build(
       content: "Example Comment",
       user: @user )
+    @delete_comment = comments(:one)
   end
 
   test "should create comment" do
@@ -14,6 +15,15 @@ class CommentCreateTest < ActionDispatch::IntegrationTest
     get advice_path(@advice)
     post advice_comments_path(@advice, params:{comment:{content: @comment.content}}, method: :POST)
     assert @comment.save
+    assert_redirected_to advice_path(@advice)
+    follow_redirect!
+    assert_template "advices/show"
+  end
+
+  test "should delete comment" do
+    log_in_as(@user)
+    get advice_path(@advice)
+    delete comment_path(@delete_comment)
     assert_redirected_to advice_path(@advice)
     follow_redirect!
     assert_template "advices/show"
