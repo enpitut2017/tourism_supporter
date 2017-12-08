292b4bdfb4b005864f2f38677801515768b741ed
Mori Atsushi <e1237@s.akashi.ac.jp>
2017-12-08 17:46:51 +0900

    検索ができるようにした

--- app/assets/javascripts/get_position.js
+++ app/assets/javascripts/get_position.js
@@ -3,21 +3,67 @@ $(function(){
 
   $('#distance-button').on('change', function () {
     if ($(this).prop('checked')) {
+      is_location_enabled = true;
       $('#distance-range-field').slideDown();
+      get_current_position();
     } else {
+      is_location_enabled = false;
       $('#distance-range-field').slideUp()
     }
   });
+
+  $('#search-submit').click(function() {
+    $('#search-form').submit();
+  });
+
+  $("#finish-detail-settings").click(function() {
+    $('#search-form').submit();
+  })
+
+  $('#search-form').submit(function(){
+    if(!is_location_enabled || is_location_geted) {
+      if(is_location_enabled) {
+        $("#distance").val($("#distance-range").val());
+        $("#lat").val(lat);
+        $("#lng").val(lng);
+      } else {
+        $("#distance").val("");
+      }
+    } else {
+      is_submited = true;
+      return false;
+    }
+  })
 });
 
-$('#location-button').click(function() {
+var is_submited = false;
+var is_location_geted = false;
+var is_location_enabled = false;
+var lat;
+var lng;
+
+var show_distance_range_field = function() {
+  if ($('#distance-button').prop('checked')) {
+    is_location_enabled = true;
+    $('#distance-range-field').show();
+    get_current_position();
+  } else {
+    is_location_enabled = false;
+    $('#distance-range-field').hide();
+  }
+};
+
+var get_current_position = function() {
   // 現在地を取得
   navigator.geolocation.getCurrentPosition(
     // 取得成功した場合
     function(position) {
-        document.getElementById('lat').value = position.coords.latitude;
-        document.getElementById('lng').value = position.coords.longitude;
-        $('#location-form').submit();
+      lat = position.coords.latitude;
+      lng = position.coords.longitude;
+      is_location_geted = true;
+      if(is_submited) {
+        $('#search-form').submit();
+      }
     },
     // 取得失敗した場合
     function(error) {
@@ -37,12 +83,4 @@ $('#location-button').click(function() {
       }
     }
   );
-});
-
-var show_distance_range_field = function() {
-  if ($('#distance-button').prop('checked')) {
-    $('#distance-range-field').show();
-  } else {
-    $('#distance-range-field').hide();
-  }
-};
+}
